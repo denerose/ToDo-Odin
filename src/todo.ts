e158ac4550ad326af05322ed396db707730e67f9
“Saphron” <saphron.hastie@gmail.com>
2023-09-07 21:33:54 +1000

    Can add tasks to DOM

--- src/todo.ts
+++ src/todo.ts
@@ -1,40 +1,57 @@
-namespace Tasks {
+export namespace Tasks {
 
-interface Person {
-    name: string
-}
-
-interface ToDoItemProps {
-    title: string;
-    description: string;
-    status: boolean;
-    assignedPerson: Person;
-}
-
-export class ToDoItem {
-
-    title: string;
-    description?: string;
-    status: boolean;
-    assignedPerson?: Person;
-
-    constructor(props: ToDoItemProps) {
-        this.title = props.title
-        this.description = props.description
-        this.status = props.status
-        this.assignedPerson = props.assignedPerson
+    interface Person {
+        name: string
     }
 
-    changeStatus() {
-        this.status = !this.status
+    const people: Person[] = [{ name: "Me" }]
+
+    interface ToDoItemProps {
+        title: string;
+        description?: string;
+        status: boolean;
+        assignedPerson?: Person;
     }
 
-}
+    export class ToDoItem {
 
-export const loungeRoom: ToDoItem [] = []
+        title: string;
+        description?: string;
+        status: boolean;
+        assignedPerson?: Person;
 
-export function getRoom() {
-    return(loungeRoom)
-}
+        constructor(props: ToDoItemProps) {
+            this.title = props.title
+            this.description = props.description
+            this.status = props.status
+            this.assignedPerson = props.assignedPerson
+        }
+
+        changeStatus() {
+            this.status = !this.status
+        }
+
+    }
+
+    let testTasks = [
+        new ToDoItem({
+            title: "test task",
+            description: "a description can go here",
+            status: true,
+            assignedPerson: { name: "Me" }
+        }),
+        new ToDoItem({
+            title: "test task 2",
+            description: "a description can go here",
+            status: false,
+            assignedPerson: { name: "You" }
+        }),
+    ]
+
+    const loungeRoom: ToDoItem[] = testTasks
+
+    export function getRoom() {
+        return (loungeRoom)
+    }
 
 }
